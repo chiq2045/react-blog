58d2d82d487c9850b13882f748a74648ae37095c
chiq2045 <charles.oroko@outlook.com>
2021-08-16 00:53:44 +0300

    add cors support

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,7 @@ import Koa from 'koa';
 import logger from 'koa-logger';
 import mount from 'koa-mount';
 import graphqlHTTP from 'koa-graphql';
+import cors from '@koa/cors';
 import { schema } from './schema';
 import { resolvers } from './resolvers';
 import { startDatabase } from './database';
@@ -10,6 +11,7 @@ const app = new Koa();
 const context = async() => await startDatabase();
 
 app.use(logger());
+app.use(cors());
 app.use(mount('/graphql', graphqlHTTP({
 	schema,
 	rootValue: resolvers,
