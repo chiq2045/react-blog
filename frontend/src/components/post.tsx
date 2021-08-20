57d9711ae6affa3e6141d4de6c874233fe4761a6
chiq2045 <charles.oroko@outlook.com>
2021-08-20 14:29:38 +0300

    update post

--- frontend/src/components/post.tsx
+++ frontend/src/components/post.tsx
@@ -1,28 +1,36 @@
 import React from 'react';
 import { PostType } from 'utils/types';
-import {formatDisplayDate} from 'utils/constants';
+import { formatDisplayDate } from 'utils/constants';
 
 interface Props {
   post: PostType;
 }
 
-export const Post = ({post}: Props) => {
+export const Post = ({ post }: Props) => {
   return (
-    <div className='tile p-1 bg-gray-200 u-round'>
-      <div className='tile__icon'>
-        <figure className='avatar'>
-          <img src='https://via.placeholder.com/200' />
-        </figure>
+    <div className='card card--slide-up'>
+      <div className='card__container'>
+        <div
+          className='card__image'
+          style={{
+            backgroundImage: 'https://unsplash.it/640/480?random&amp;blur'
+          }}
+        ></div>
       </div>
-      <div className='tile__container'>
-        <p className='tile__title m-0'>
-          {post.title}
-        </p>
-        <p className='tile__subtitle m-0'>
-          {post.content}
-        </p>
-        <span className='info'>{formatDisplayDate(post.dateModified ? `(Edited: ${post.dateModified})` : post.dateCreated)}</span>
+      <div className='card__mobile-title'>
+        <div className='content'>
+          <div className='tile'>
+            <div className='tile__container'>
+              <p className='tile__title'>{post.title}</p>
+              {/* <p className='tile__subtitle'>By John Doe</p> */}
+            </div>
+          </div>
+        </div>
       </div>
+      <div className='card__body content'>
+        {post.content}
+      </div>
+      <div className='card__footer content'>{post.dateModified ? `Modified: ${formatDisplayDate(post.dateModified)}` : formatDisplayDate(post.dateCreated)}</div>
     </div>
   );
 };
