9795f6b04fa67018bc2e8b5fafbfa71c64c5cfff
chiq2045 <charles.oroko@outlook.com>
2021-10-25 14:53:48 +0300

    move to online database

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,3 +1,6 @@
+/* eslint-disable import/first */
+import { config } from 'dotenv';
+config();
 import Koa from 'koa';
 import logger from 'koa-logger';
 import mount from 'koa-mount';
@@ -5,18 +8,24 @@ import graphqlHTTP from 'koa-graphql';
 import cors from '@koa/cors';
 import { schema } from './schema';
 import { resolvers } from './resolvers';
-import { startDatabase } from './database';
+import { db } from './database';
+
 
 const app = new Koa();
-const context = async() => await startDatabase();
+const context = async () => await db();
 
 app.use(logger());
 app.use(cors());
-app.use(mount('/graphql', graphqlHTTP({
-	schema,
-	rootValue: resolvers,
-	graphiql: true,
-	context
-})));
+app.use(
+  mount(
+    '/graphql',
+    graphqlHTTP({
+      schema,
+      rootValue: resolvers,
+      graphiql: true,
+      context
+    })
+  )
+);
 
-export {app};
+export { app };
--- backend/src/schema.ts
+++ backend/src/schema.ts
@@ -1,15 +1,6 @@
 import { buildSchema } from 'graphql';
 
 export const schema = buildSchema(`
-	type User {
-		id: ID!
-		name: String
-		username: String!
-		email: String!
-		dateJoined: String!
-		posts: [Post!]
-	}
-
 	type Post {
 		id: ID!
 		title: String!
@@ -22,15 +13,11 @@ export const schema = buildSchema(`
 	  hello: String
 		posts: [Post!]!
 		post(id: Int!): Post!
-		users: [User!]!
-		user(id: Int!): User!
   }
 
 	type Mutation {
 		editPost(id: Int!, title: String!, content: String!): Post!
-		editUser(id: Int!, name: String, username: String!, email: String!): User!
 		addPost(title: String!, content: String!): Post!
-		addUser(name: String, email: String!, username: String!): User!
 	}
 `);
 
