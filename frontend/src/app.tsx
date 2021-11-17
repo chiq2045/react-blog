18d1338e61ec07aec1b14ccdc214ed3ce69c7e24
chiq2045 <charles.oroko@outlook.com>
2021-11-17 17:23:17 +0300

    update app

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -5,35 +5,52 @@ import { AddPost } from 'views/addPost';
 import { ViewPosts } from 'views/viewPosts';
 import { ViewPost } from 'views/viewPost';
 import { EditPost } from 'views/editPost';
-import { usePosts } from 'utils/hooks/usePosts';
+import { GET_POSTS } from 'utils/constants';
+import { useQuery } from '@apollo/client';
+import { useToasts } from 'utils/hooks/useToasts';
+import { Toast } from 'components/toast';
+import { Toast as ToastType } from 'utils/types';
+import {Home} from 'views/Home';
 
 export const App = () => {
-  const { savePost, saveDraft, error, loading } = usePosts();
+  const { toasts, addToast, removeToast } = useToasts();
+  const { loading, error, data } = useQuery(GET_POSTS, {
+    onError: (e) =>
+      addToast({
+        id: e.name,
+        value: e.message
+      })
+  });
+
+  const showToast = (toast: ToastType, index: number) => (
+    <Toast
+      key={toast.id}
+      value={toast.value}
+      onClose={() => removeToast(toast.id)}
+      index={index}
+    />
+  );
+
   return (
     <Router>
       <NavBar />
+      {toasts.length > 0 && toasts.map(showToast)}
       <main className='padtop'>
         <div className='space xlarge' />
-        <div className='space xlarge' />
+        <div className='space large' />
         <div className='content'>
           <Switch>
             <Route exact path='/'>
-              Blog
+              <Home posts={data?.posts ?? []} />
             </Route>
             <Route exact path='/posts'>
-              <ViewPosts />
+              <ViewPosts posts={data?.posts} loading={loading} error={error} />
             </Route>
             <Route path='/posts/edit/:id'>
               <EditPost />
             </Route>
             <Route path='/posts/new'>
-              <AddPost
-                saveDraft={saveDraft}
-                savePost={savePost}
-                error={error.saveError ?? error.draftError ?? undefined}
-                isSavingDraft={loading.isSavingDraft}
-                isSavingPost={loading.isSavingPost}
-              />
+              <AddPost addToast={addToast} />
             </Route>
             <Route path='/posts/view/:id'>
               <ViewPost />
--- frontend/src/views/viewPosts.tsx
+++ frontend/src/views/viewPosts.tsx
@@ -1,5 +1,58 @@
-import React from 'react';
+import { ApolloError } from '@apollo/client';
+import React, { useMemo } from 'react';
+import { Post } from 'utils/types';
+import sanitizeHtml from 'sanitize-html';
+import striptags from 'striptags';
 
-export const ViewPosts = () => {
-  return <div>View All Posts</div>;
+interface Props {
+  posts: Array<Post> | undefined;
+  loading: boolean;
+  error: ApolloError | undefined;
+}
+export const ViewPosts = ({ posts, loading, error }: Props) => {
+  const postsArray = useMemo(() => {
+    return Object.values(posts ?? []);
+  }, [posts]);
+
+  const displaySummary = (htmlString: string, maxCharacterCount =200) => {
+    const text = striptags(sanitizeHtml(htmlString)).trim();
+    if (text.length < maxCharacterCount) {
+      return text;
+    }
+    return `${text.substring(0, maxCharacterCount)}...`;
+  };
+  return (
+    <>
+      <h1>Overview</h1>
+      {loading ? (
+        <div className='animated loading hide-text'>Loading...</div>
+      ) : (
+        <ul className='no-bullets'>
+          {postsArray.length > 0 &&
+            postsArray.map((p) => {
+              return (
+                <li key={p.id}>
+                  <div className='card'>
+                    <div className='content'>
+                      {console.log(p)}
+                      <p className='title'>{p.title}</p>
+                      <p className='subtitle'>
+                        {new Date(
+                          parseInt(
+                            p.dateModified ? p.dateModified : p.dateCreated
+                          )
+                        ).toDateString()}
+                      </p>
+                      <p>
+                        {displaySummary(p.content)}
+                      </p>
+                    </div>
+                  </div>
+                </li>
+              );
+            })}
+        </ul>
+      )}
+    </>
+  );
 };
